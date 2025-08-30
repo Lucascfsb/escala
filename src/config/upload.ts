ec1d6c2c01c385d07d071fc18cb1f98479fd6c11
Lucas Castro <lucascfsb@gmail.com>
2025-08-30 16:26:50 -0300

    Atualização do arquivo upload.ts e package.json

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,38 +1,44 @@
-import crypto from "node:crypto";
-import path from "node:path";
-import multer from "multer";
-import type { StorageEngine } from "multer";
+import crypto from 'node:crypto'
+import fs from 'node:fs'
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+import path from 'node:path'
+import multer from 'multer'
+import type { StorageEngine } from 'multer'
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
+
+if (!fs.existsSync(tmpFolder)) {
+  fs.mkdirSync(tmpFolder)
+}
 
 interface IUploadConfig {
-  driver: "s3" | "disk";
-  directory: string;
-  tmpFolder: string;
-  storage: StorageEngine;
+  driver: 's3' | 'disk'
+  directory: string
+  tmpFolder: string
+  storage: StorageEngine
   config: {
     aws?: {
-      bucket: string;
-    };
-  };
+      bucket: string
+    }
+  }
 }
 
 export const uploadConfig: IUploadConfig = {
-  driver: (process.env.STORAGE_DRIVER as "s3" | "disk") || "disk",
+  driver: (process.env.STORAGE_DRIVER as 's3' | 'disk') || 'disk',
   directory: tmpFolder,
   tmpFolder: tmpFolder,
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}-${file.originalname}`;
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
 
-      return callback(null, fileName);
+      return callback(null, fileName)
     },
   }),
   config: {
     aws: {
-      bucket: process.env.AWS_BUCKET_NAME || "",
+      bucket: process.env.AWS_BUCKET_NAME || '',
     },
   },
-};
+}
